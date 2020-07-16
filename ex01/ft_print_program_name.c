06c3f7efa532eb395d9b061bbec4998621b52650
Cameron Sassman <csassman@c3r8s5.wethinkcode.co.za>
2020-07-16 16:38:19 +0200

    ft_print_program_name

---
+++ ex01/ft_print_program_name.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csassman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/16 16:05:51 by csassman          #+#    #+#             */
+/*   Updated: 2020/07/16 16:37:19 by csassman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1 ,&c,1);
+}
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	(void)argc;
+	ft_putstr(argv[30] = "ft_print_program_name");
+	ft_putchar('\n');
+	return (0);
+}
